e183cbf7cd637161435ed67882f73d9941678326
Tyskern <104502845+Tyskern@users.noreply.github.com>
2025-01-25 23:14:46 +0100

    t

---
+++ Source/PPFGame/PpfDelegates.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PpfDelegates.h"
+
+
+PpfDelegates::PpfDelegates()
+{
+}
+
+PpfDelegates::~PpfDelegates()
+{
+}
