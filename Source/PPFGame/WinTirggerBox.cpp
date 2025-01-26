85a0a9f99ff1181d56d80fe2adc2d7b3853ade24
Mathias Mohn Mørch <mathmoe64@gmail.com>
2025-01-26 14:02:40 +0100

    vfx working

---
+++ Source/PPFGame/WinTirggerBox.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WinTirggerBox.h"
+
+#include "Materials/MaterialParameterCollectionInstance.h"
+#include "Player/PPFPlayerPawn.h"
+
+
+// Sets default values
+AWinTirggerBox::AWinTirggerBox()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AWinTirggerBox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AWinTirggerBox::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	if (!OtherActor->IsA<APPFPlayerPawn>())
+	{
+		return;
+	}
+
+	FVector2D ScreenPosition;
+	GetWorld()->GetFirstPlayerController()->ProjectWorldLocationToScreen(OtherActor->GetActorLocation(), ScreenPosition);
+	
+	GetWorld()->GetParameterCollectionInstance(m_GlobalMPC)->SetVectorParameterValue("PlayerScreenPosition", FVector(ScreenPosition, 0));
+}
+
+// Called every frame
+void AWinTirggerBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
